9cf8112b7775b7c4798f55618c2f20dac81ab192
ValterBarros <valterinside@gmail.com>
2017-11-13 23:03:08 -0300

    master-enhancements-on-view
    - Generate views from devise
    - Add login devise to root path
    - Redirect login success devise to tasks_path
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def after_sign_in_path_for(resource)
+    tasks_path  
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end

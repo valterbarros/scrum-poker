161f98cbf0cea12b2d9fe0549d969389e09cfd7e
ValterBarros <valterinside@gmail.com>
2017-11-18 16:49:15 -0300

    feature/VB0163-add-new-concern-to-controller-to-treat-with-votation-count
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
+  include Votation
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
-    rooms_path
-  end
+  def after_sign_in_path_for(resource);rooms_path;end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
+
+  def current_session_vote_id
+    session[:session_vote]
+  end
+
+  def set_current_session_vote_id(id)
+    session[:session_vote]
   end
 
 end

e387c75e0ca37a3cb95c137ba9cc3fa015d31357
ValterBarros <valterinside@gmail.com>
2017-11-18 11:46:09 -0300

    feature/VB0163-add-show-action-to-rooms-controller
    - Create action and add some html content
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,17 @@
 class RoomsController < ApplicationController
+  before_action :set_session_vote, only: [:show]
+
   def index
     @session_votes = SessionVote.all
+  end
+
+  def show
+    @invite = Invite.new
+    @users = User.all
+  end
+
+  private
+  def set_session_vote
+    @session_vote = SessionVote.find(params[:id])
   end
 end

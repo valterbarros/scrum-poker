e2c34dd6de2940ce90b9371a18af7e97ed3d8318
ValterBarros <valterinside@gmail.com>
2017-11-18 11:47:08 -0300

    feature/VB0163-create-controller-invites
    - Create controller to create a new invite and create a notification to user
    - Add show page to rooms
    - Add scss to rooms
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

---
+++ app/controllers/invites_controller.rb
@@ -0,0 +1,20 @@
+class InvitesController < ApplicationController
+  def create
+    @invite = Invite.new(invite_params)
+    @invite.sender = current_user
+    if @invite.save
+      @notification = Notification.new(
+        title: "You are invited for a new session by #{current_user.name}",
+        body: "Do you want accept the invite?",
+        user_id: invite_params[:recipient_id]
+      )
+      @notification.save
+    end
+  end
+  
+  private
+
+  def invite_params
+    params.require(:invite).permit(:recipient_id, :session_vote_id)
+  end
+end

88c20269d9e1d848090ed3f1a0390cbda7eaab15
ValterBarros <valterinside@gmail.com>
2017-11-18 19:00:16 -0300

    feature/VB0163-changes-on-view
    - Add js to treat with steps on rooms view
    - Extract css from step to a file css
    - Add title to step migration
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

---
+++ app/assets/javascripts/init.coffee
@@ -0,0 +1,5 @@
+window.App ||= {}
+
+App.init = ->
+  # Steps
+  new Steps().init()
--- db/migrate/20171116022110_create_step.rb
+++ db/migrate/20171116022110_create_step.rb
@@ -1,6 +1,7 @@
 class CreateStep < ActiveRecord::Migration[5.1]
   def change
     create_table :steps do |t|
+      t.string :title
       t.references :session_vote, index: true, foreign_key: true
 
       t.timestamps

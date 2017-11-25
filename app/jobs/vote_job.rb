161fd0e8d483c7e1f0465b444d25d7e96879fcc5
ValterBarros <valterinside@gmail.com>
2017-11-25 13:17:44 -0300

    feature/VB0163-changes-on-view
    - Implement vote_success and vote_fail to add class confirmed and active to cards when user vote in one card
    - Changes on vote job
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/jobs/vote_job.rb
+++ app/jobs/vote_job.rb
@@ -1,21 +1,10 @@
 class VoteJob < ApplicationJob
-  attr_reader :vote, :session_vote_id, :step_id, :user_id
-
-  def initialize(params = {}) 
-    super
-    @vote = params[:vote]
-    @step_id = params[:step].id
-    @user_id = @vote.user.id
-    @session_vote_id = vote.session_vote.id 
-  end
-
-  def perform(argument = {})
+  def perform(vote)
     ActionCable.server.broadcast(
-      "rooms:#{session_vote_id}", 
-      user_id: user_id, 
+      "rooms:#{vote.session_vote.id}", 
+      user_id: vote.user.id, 
       score: vote.score,
-      step_id: step.id,
-      session_vote_id: session_vote_id
+      session_vote_id: vote.session_vote.id
     )
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :session_vote
   belongs_to :task
   belongs_to :user
+
+  after_commit { VoteJob.perform_later(self) }
 end

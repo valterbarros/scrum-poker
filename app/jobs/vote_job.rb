87270134182652d69694c4ef4e74349375e91232
ValterBarros <valterinside@gmail.com>
2017-11-25 10:20:58 -0300

    feature/VB0163-clean-up-some-files
    - Remove some files from this branch because are a task so complicated and need a specify branch for them
    - Keep some old changes
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/jobs/vote_job.rb
+++ app/jobs/vote_job.rb
@@ -1,16 +1,21 @@
 class VoteJob < ApplicationJob
-  attr_reader :vote, :session_vote_id
+  attr_reader :vote, :session_vote_id, :step_id, :user_id
 
-  def initialize(vote) 
+  def initialize(params = {}) 
     super
-    @vote = vote
+    @vote = params[:vote]
+    @step_id = params[:step].id
+    @user_id = @vote.user.id
     @session_vote_id = vote.session_vote.id 
   end
 
   def perform(argument = {})
     ActionCable.server.broadcast(
       "rooms:#{session_vote_id}", 
-      user: vote.user.name, score: vote.score
+      user_id: user_id, 
+      score: vote.score,
+      step_id: step.id,
+      session_vote_id: session_vote_id
     )
   end
 end

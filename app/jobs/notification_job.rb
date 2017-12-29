e4f95dc3a68b524c68386b0f6373c5fe464b5806
ValterBarros <valterinside@gmail.com>
2017-12-29 00:39:41 -0300

    feature/VB0469-implement-add-user-to-view-when-him-enter-on-room
    - Create a new active job for participant for create a broadcast
    - Add broadcast from participant on controller
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -2,7 +2,7 @@ class NotificationJob < ApplicationJob
   def perform(notification, user_id)
     ActionCable.server.broadcast(
       "notifications:#{user_id}",
-      partial: RoomsController.render(partial: 'layouts/notification_item', locals: { notification: notification })
+      partial: ApplicationController.render(partial: 'layouts/notification_item', locals: { notification: notification })
     )
   end
 end

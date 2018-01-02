057b78d9c6e7a34ed320018f7552c3df57f73d95
ValterBarros <valterinside@gmail.com>
2018-01-01 22:53:02 -0300

    feature/VB0469-some-refactors-to-clean-up-controller
    - Rename specs from controllers folder
    - Add service object to deal with show action for owner and participant room
    - Call this service on controller removing so many ifs
    - Add a alias to call session_vote_id as room_id on user model
    
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ScrumPoker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths += %W(#{config.root}/app)
+    config.autoload_paths += %W(#{config.root}/app services)
     config.action_cable.mount_path = '/websocket'
   end
 end

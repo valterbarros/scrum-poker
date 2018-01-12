176d60924113468e409ba71364af080942f71902
ValterBarros <valterinside@gmail.com>
2018-01-11 23:55:00 -0300

    feature/VB0469-refactors-on-show-action-from-rooms-and-onrganize-tests
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/queries/query_user_for_invite.rb
+++ app/queries/query_user_for_invite.rb
@@ -6,13 +6,20 @@ module Queries
     end
 
     def fetch_result
-      (user_invite_is_not_the_same + all_in_diferent_room_and_without_room_id).uniq - [owner_from_session]
+      (user_invite_is_not_the_same + all_in_diferent_room_and_without_room_id)
+        .uniq - [owner_from_session, user_invite_is_the_same].flatten
     end
 
     def user_invite_is_not_the_same
       @relation.distinct
         .joins('inner join invites on users.id = invites.recipient_id')
         .where('invites.session_vote_id != ?', @id)
+    end
+
+    def user_invite_is_the_same
+      @relation.distinct
+        .joins('inner join invites on users.id = invites.recipient_id')
+        .where('invites.session_vote_id = ?', @id)
     end
 
     def all_in_diferent_room_and_without_room_id
--- spec/queries/query_user_for_invite_spec.rb
+++ spec/queries/query_user_for_invite_spec.rb
@@ -5,26 +5,28 @@ describe Queries::QueryUserForInvite, type: %i(users queries) do
   let(:params) {  }
   let(:result) {  }
 
-  context 'When tow users one is owner and other has no room' do
-    let(:room_002) { create(:session_vote) }
-    let(:user_003) { create(:user) }
-    let(:invite) { create(:invite, recipient: user_003, sender: user_002, session_vote: room_002) }
+  context 'When has four users one is owner, other is invited to a room, and two has no room' do
+    let(:user) { create(:user) }
 
+    let(:room_002) { create(:session_vote) }
+
+    let(:invite) { create(:invite, recipient: user_003, sender: user_002, session_vote: room) }
+    let(:user_003) { create(:user) }
     let(:user_002) { create(:user) }
     let(:room) { create(:session_vote, owner: user_002) }
 
-    let(:user) { create(:user) }
 
     let(:params) { { id: room.id } }
     let(:result) { subject.fetch_result }
 
     before do
       invite
+      room_002
       user
     end
 
-    it 'result should be [user]' do
-      expect(result).to match_array [user, user_003, room_002.owner]
+    it 'result should be [user, room_002.owner]' do
+      expect(result).to match_array [user, room_002.owner]
     end
   end
 end

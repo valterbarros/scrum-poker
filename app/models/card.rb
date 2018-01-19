0ddc884c08cb0774f0fa55976debc45977174d63
Arthur Novaes <arthur.novaes@stant.com.br>
2018-01-19 11:23:38 -0300

    feature/AN1227-add-annotation Add annotate to project
    -
    
    Signed-off-by: Arthur Novaes <arthur.novaes@stant.com.br>

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: cards
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Card < ApplicationRecord
   has_and_belongs_to_many :steps
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: steps
+#
+#  id              :integer          not null, primary key
+#  session_vote_id :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_steps_on_session_vote_id  (session_vote_id)
+#
+
 class Step < ApplicationRecord
   belongs_to :session_vote
 

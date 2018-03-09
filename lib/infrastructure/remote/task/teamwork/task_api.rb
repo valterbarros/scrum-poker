e5f5500f047587607e965de01407b1c45e77fe80
Rachid Calazans <rachidcalazans@gmail.com>
2018-03-08 21:19:09 -0300

    feature/RC#32 Refactor TaskApi for Teamwork
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/infrastructure/remote/task/teamwork/task_api.rb
+++ lib/infrastructure/remote/task/teamwork/task_api.rb
@@ -1,32 +1,30 @@
-require 'http'
-require_relative '../teamwork/dto/task_response.rb'
-
 module Infrastructure
   module Remote
     module Task
       module Teamwork
         class TaskApi
-
-          def initialize(request_api:)
-            @request_api = request_api
-          end
+          include ::Infrastructure::Remote::ApiBase
 
           def all_by(project_id:)
-            action = "https://rogalabs.teamwork.com/tasklists/#{project_id}/tasks.json"
-            r = JSON.parse(HTTP
-              .headers(@request_api.headers)
-              .get(action).body)['todo-items'][0]
-              #.headers('Authorization': "Basic #{token}")
+            action    = "#{request_api.url_base}/tasklists/#{project_id}/tasks.json"
+            response  = basic_auth.get(action)
+            json_body = JSON.parse(response.body)
 
-            Dto::TaskResponse.new(r)
+            todo_items = json_body.fetch('todo-items', [])
+
+            todo_items.map do |item|
+              dto = Dto::TaskResponse.new(item)
+              ::Domain::Entity::Task.new(dto.entity_attributes)
+            end
+
           end
 
+          private
+          def basic_auth
+            HTTP.basic_auth(user: request_api.user, pass: '')
+          end
         end
       end
-
     end
   end
 end
-
-#api = Infrastructure::Remote::Task::Teamwork::TaskApi.new
-#puts api.all_by(project_id: 551857).content

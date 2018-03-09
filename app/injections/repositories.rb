97d0663a2c47b93a3aff2211166046ce4c6d9b05
Rachid Calazans <rachidcalazans@gmail.com>
2018-03-08 21:19:25 -0300

    feature/RC#32 Refactor InjectionsRepositories
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- app/injections/repositories.rb
+++ app/injections/repositories.rb
@@ -1,15 +1,25 @@
 module Injections
   module Repositories
 
+    def build_teamwork_adapter(url_base:, token:)
+      Adapter.new(:teamwork, url_base, token, '')
+    end
+
+    def build_trello_adapter(url_base:, token:, key:)
+      Adapter.new(:teamwork, url_base, token, key)
+    end
+
     def provide_task_repo(adapter:)
       TASK_REPO[adapter.name].call(adapter)
     end
 
     private
-    RequestApi = Struct(:headers, :body_params)
+    Adapter = Struct.new(:name, :url_base, :token, :key)
+
+    RequestApi = Struct.new(:url_base, :headers, :body_params, :user)
     REQUESTS = {
-      teamwork: -> (adapter) { RequestApi.new(headers: {'Authorization': "Basic #{adapter.token}"}, body_params: {}) },
-      trello:   -> (adapter) { RequestApi.new(headers: {}, body_params: "key=#{adapter.key}&token=#{adapter.token}") }
+      teamwork: -> (adapter) { RequestApi.new(adapter.url_base, {}, {}, adapter.token) },
+      trello:   -> (adapter) { RequestApi.new(adapter.url_base, {}, "key=#{adapter.key}&token=#{adapter.token}") }
     }
 
     TASK_REPO = {
@@ -18,10 +28,3 @@ module Injections
     }
   end
 end
-
-# trello token: 75bce39b08cc77d16bc652c24540665851fdc4763c7dda183570857ab8fe3abc
-# trello key: 92eab9af2b2a2e2066940fb5796490df
-# curl 'https://api.trello.com/1/members/me/boards?key=92eab9af2b2a2e2066940fb5796490df&token=75bce39b08cc77d16bc652c24540665851fdc4763c7dda183570857ab8fe3abc'
-#
-# curl 'https://api.trello.com/1/members/me/boards?key=92eab9af2b2a2e2066940fb5796490df&token=75bce39b08cc77d16bc652| 18
-# ▸ db/                         |~'

a59086ab3daa02cf0ccf99a5095c0a6bca9c838c
valterbarros <valterinside@gmail.com>
2018-03-19 16:05:28 -0300

    add-logout-button
    Signed-off-by: valterbarros <valterinside@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
   devise_scope :user do
     root to: "devise/sessions#new"

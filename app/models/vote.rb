7aa0e70f053d03619f4df5c81053e978013ad89a
ValterBarros <valterinside@gmail.com>
2018-03-24 10:23:31 -0300

    feature/VB1068-add-validation-to-block-vote-when-room-is-not-in-progress-and-add-validation-locales
    Signed-off-by: ValterBarros <valterinside@gmail.com>

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -20,9 +20,21 @@
 class Vote < ApplicationRecord
   attr_accessor :step_position
 
+  alias_attribute :room, :session_vote
+  alias_attribute :room_id, :session_vote_id
+
   belongs_to :session_vote
   belongs_to :task
   belongs_to :user
 
   after_commit { VoteJob.perform_later(self, step_position) }
+
+  validate :room_status_is_valid?
+
+  private
+
+  def room_status_is_valid?
+    errors.add(:room_id, I18n.t(:room_invalid_status, scope: 'activerecord.errors.vote')) \
+      unless room.in_progress? 
+  end
 end

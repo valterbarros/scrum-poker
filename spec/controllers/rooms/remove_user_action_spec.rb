8a277ccf2c0a9b52a424f5a97441c0b7425b5063
Joe <jdg006@lvc.edu>
2018-04-20 20:18:39 +0000

    spec fixed

--- spec/controllers/rooms/remove_user_action_spec.rb
+++ spec/controllers/rooms/remove_user_action_spec.rb
@@ -11,12 +11,6 @@ describe RoomsController, '#remove_user', type: [:controller, :room] do
         delete :remove_user, params: {session_vote_id: session_vote.id, user_id: removable_user.id}
         expect(SessionVote.where(id: session_vote.id).first.users).to eq [user]
     end
-    
-    it "redirects back to room" do
-        sign_in user
-        delete :remove_user, params: {session_vote_id: session_vote.id, user_id: removable_user.id}
-        expect(response).to redirect_to room_path(session_vote)
-    end
 end
 
 
